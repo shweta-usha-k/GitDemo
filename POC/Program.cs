7d38c18da0c9320c5a911b04a4b5714e1f77459c
usha-kakde-amla <124877555+shweta-usha-k@users.noreply.github.com>
2023-02-13 15:39:49 +0530

    COmmit
    COmmit

---
+++ POC/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder(). 
+    AddUserSecrets<Program>();
+
+string apiKey = config["apikey"];
+
+Console.WriteLine(apiKey);
